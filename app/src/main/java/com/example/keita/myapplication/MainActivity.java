a7b8af695008a6297ae6f22bd8d26c1d6723dbe3
sugikeitter <s.l.n.f.13_kei.s@me.com>
2018-02-10 17:54:00 +0900

    Add sending int data

--- app/src/main/java/com/example/keita/myapplication/MainActivity.java
+++ app/src/main/java/com/example/keita/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String EXTRA_INT = "com.example.myfirstapp.INT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // TODO
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
+        int i = 999;
         intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_INT, i);
         startActivity(intent);
     }
 }
